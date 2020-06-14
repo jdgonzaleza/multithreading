3b73fedc270bf43198e937dd3cd708f3b5e813fc
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2020-06-14 18:36:13 -0500

    countdownlatch

---
+++ src/threading/countdownlatch/App.java
@@ -0,0 +1,24 @@
+package threading.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+	public static void main(String[] args) {
+		CountDownLatch latch = new CountDownLatch(3);
+
+		ExecutorService service = Executors.newFixedThreadPool(3);
+		for(int i = 0 ; i<3; i++) {
+			service.submit(new Worker(latch));
+		}
+
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Completed");
+	}
+}
---
+++ src/threading/countdownlatch/Worker.java
@@ -0,0 +1,22 @@
+package threading.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Worker implements Runnable{
+	private CountDownLatch latch;
+
+	public Worker(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+	public void run() {
+		System.out.println("Starting");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		latch.countDown();
+	}
+}
