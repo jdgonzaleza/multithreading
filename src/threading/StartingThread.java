774c65c7ca4dbad2ffbc55583e1609ed08c8806d
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2020-06-14 16:08:28 -0500

    starting a thread

---
+++ src/threading/StartingThread.java
@@ -0,0 +1,22 @@
+package threading;
+
+public class StartingThread {
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread( () -> {
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Hello from t1 " + i);
+			}
+		} );
+
+		Thread t2 = new Thread( () -> {
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Hello from t2 " + i);
+			}
+		} );
+
+		t1.start();
+		t2.start();
+	}
+}
