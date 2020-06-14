411b753831ec324b2e9a08adc46e565c9aac3016
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2020-06-14 17:01:42 -0500

    synchronized

---
+++ src/threading/Sync.java
@@ -0,0 +1,40 @@
+package threading;
+
+public class Sync {
+	private int count = 0;
+
+	public static void main(String[] args) {
+		Sync app = new Sync();
+		app.doSomething();
+	}
+
+	public synchronized void increment() {
+		count++;
+	}
+
+	public void doSomething() {
+		Thread t1 = new Thread(() -> {
+			for(int i = 0; i<10000; i++) {
+				increment();
+			}
+		});
+
+		Thread t2 = new Thread(() -> {
+			for(int i = 0; i<10000; i++) {
+				increment();
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(count);
+	}
+}
