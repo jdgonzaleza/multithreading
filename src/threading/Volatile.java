a3630486c946d31645815adbebb6e7822fe2066d
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2020-06-14 16:51:58 -0500

    volatile

---
+++ src/threading/Volatile.java
@@ -0,0 +1,30 @@
+package threading;
+
+public class Volatile {
+	public static void main(String[] args) throws InterruptedException {
+		Runner r1 = new Runner();
+		r1.start();
+
+		r1.shutdown();
+
+	}
+}
+
+class Runner extends Thread {
+	private boolean running = true;
+	public void run() {
+		while(running) {
+			System.out.println("Inside while loop 😄");
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void shutdown() {
+		running = false;
+	}
+}
